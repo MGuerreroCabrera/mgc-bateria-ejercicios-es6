9a44a7eaaad40635c23c93e9d6313f29181ea9f7
Manuel <manuel@guerrerocabrera.com>
2023-12-01 13:13:30 +0100

    fetch: ejercicio 6

--- ejercicio-6.js
+++ ejercicio-6.js
@@ -8,3 +8,14 @@ const streamers = [
 	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
 	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
 ];
+
+
+const filter = (e) => {
+    const filtered = streamers.filter((streamer) => streamer.name.toLowerCase().includes(e.target.value));
+    console.log(filtered);
+}
+
+
+const input = document.querySelector("input");
+
+input.addEventListener("input", filter);
